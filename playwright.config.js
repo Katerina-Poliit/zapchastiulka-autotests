47232e2970fcebf6d3b2ec1845de64fc42b2cceb
Katerina Poliit <alexispoliyit@gmail.com>
2024-05-03 16:02:34 +0300

    check

--- playwright.config.js
+++ playwright.config.js
@@ -66,10 +66,10 @@ module.exports = defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
