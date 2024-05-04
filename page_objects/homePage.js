5356c29ec3ca5ba726c4ee34c0fe0378da3af95e
AlexUA2022 <avsaponov@gmail.com>
2024-05-04 09:35:00 +0300

    RF_POM | header.spec | TC 01.01.2 Verify that website has the "Каталог" button

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -5,7 +5,8 @@ class HomePage {
   }
 
   locators = {
-    getLogo: () => this.page.locator('div').filter({ hasText: /^КаталогВведіть пошукове слово$/ }).getByRole('img').first()
+    getLogo: () => this.page.locator('div').filter({ hasText: /^КаталогВведіть пошукове слово$/ }).getByRole('img').first(),
+	 getCatalogbutton: () => this.page.getByRole('button', { name: 'Каталог' })
   };
 
   async open() {
