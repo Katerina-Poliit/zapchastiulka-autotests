87a5cf5389a4105adf2c4e9a3e73014d91989d16
AlexUA2022 <avsaponov@gmail.com>
2024-05-04 14:18:06 +0300

    RF_POM | header.spec | TC 01.01.1 Verify that the "Каталог" menu contains the "Запчастини до сільгосптехніки" button and the vector

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -8,7 +8,9 @@ class HomePage {
     getLogo: () => this.page.locator('div').filter({ hasText: /^КаталогВведіть пошукове слово$/ }).getByRole('img').first(),
 	 getCatalogbutton: () => this.page.getByRole('button', { name: 'Каталог' }),
 	 getdropdownMenu: () => this.page.getByRole('banner').getByRole('list'),
-    getFilterСhapter: () => this.page.getByRole('button', { name: 'Фільтри' })
+    getFilterСhapter: () => this.page.getByRole('button', { name: 'Фільтри' }),
+	 getSparePartsForAgriculturalMachinery: () => this.page.getByRole('button', { name: 'Запчастини до сільгосптехніки' }),
+	 getSparePartsForAgriculturalMachineryVector: () => this.page.locator('.w-full .stroke-iconPrimary').nth(2)
   };
 
   async open() {
