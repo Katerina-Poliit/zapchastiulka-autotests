9f72ab85e64fd1f7424e42a5672825e2605c3912
AlexUA2022 <avsaponov@gmail.com>
2024-05-05 07:46:55 +0300

    RF_POM | header.spec | TC 01.01.8 Verify that the subcategory is opened after clicking the "Запчастини до сільгосптехніки" button

--- helpers/testData.js
+++ helpers/testData.js
@@ -3,8 +3,9 @@ export const BASE_URL = 'https://zapchaztiulka-catalog-frontend.vercel.app/';
 
 //test data
 export const HEADER_CATALOG_BUTTON_TEXT = 'Каталог';
-export const EXPECTED_ITEMS = ['Масла та автохімія', 'Фільтри', 'Запчастини до сільгосптехніки', 'Запчастини до вантажних автомобілів', 'Підшипники', 'Інші товари', 'Шини та камери'];
- export const FILTER_SUBCATEGORY = ['Масляні', 'Паливні', 'Повітряні','Інші'];
+export const SPARE_PARTS_FOR_AGRICULTURAL_MACHINERY = ['Масла та автохімія', 'Фільтри', 'Запчастини до сільгосптехніки', 'Запчастини до вантажних автомобілів', 'Підшипники', 'Інші товари', 'Шини та камери'];
+export const FILTER_SUBCATEGORY = ['Масляні', 'Паливні', 'Повітряні','Інші'];
+export const SPARE_PARTS_FOR_AGRICULTURAL_MACHINERY_SUBCATEGORY = ['John Deere', 'Case та New Holland', 'Claas','МТЗ', 'Інша сільгосптехніка'];
 //login  credential
 
 
