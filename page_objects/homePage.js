7b4974fd70946860c71d112b3b92f538e5df95e5
AlexUA2022 <avsaponov@gmail.com>
2024-05-15 11:49:42 +0300

    Fixed TC 03.01.35

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -88,7 +88,7 @@ class HomePage {
 		getFilterPriceMin: () => this.page.getByPlaceholder('4'),
 		getFilterPriceMax: () => this.page.getByPlaceholder('000'),
 		getFilterPriceDropdown: () => this.page.locator('div').filter({ hasText: /^Ціна—$/ }).getByRole('button'),
-		getProductListPage: (item) => this.page.getByText('ФільтрСортувати:Оберіть значення...ВідсутнійАртикул: testProduct1')
+		getProductListPage: (item) => this.page.locator('ul').filter({ hasText: 'Артикул: 667248.0Пас ротора' })
 		};
 
 	async open() {
