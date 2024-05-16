fb7bce17e3c7e34745cbfbd8edb38a62f3fe5613
AlexUA2022 <avsaponov@gmail.com>
2024-05-16 16:30:02 +0300

    RF_POM | header.spec | TC 03.01.60 Verify that the “Бажаєте щось особливе?” dialog box contains the "Розкажіть нам про ваші побажання. А про інше потурбується менеджер." description

--- page_objects/doYouWantSomethingSpecialDialogBoxPage.js
+++ page_objects/doYouWantSomethingSpecialDialogBoxPage.js
@@ -6,7 +6,8 @@ class DoYouWantSomethingSpecialDialogBoxPage {
  locators = {
 		  getDoYouWantSomethingSpecialDialogBoxPageHeader: () => this.page.getByRole('heading', { name: 'Бажаєте щось особливе?' }),
 		  getCloseButton: () => this.page.locator('#modal-root').getByRole('button').first(),
-		  getWrenchIcon: () => this.page.locator('.modal-body > div > div')
+		  getWrenchIcon: () => this.page.locator('.modal-body > div > div'),
+		  getDescription: () => this.page.getByText('Розкажіть нам про ваші побажання. А про інше потурбується менеджер')
  };
 
  async clickCloseButton() {
