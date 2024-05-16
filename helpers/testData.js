a3b4cabe497a1dd263f11004cd6ae9479b0b0fe4
AlexUA2022 <avsaponov@gmail.com>
2024-05-16 16:47:36 +0300

    RF_POM | header.spec | TC 03.01.61 Verify that the “Бажаєте щось особливе?” dialog box contains the "Номер телефону" field

--- helpers/testData.js
+++ helpers/testData.js
@@ -113,6 +113,7 @@ export const X_OCHUSTUTU_BUTTON_TEXT = 'Очистити';
 export const LEARN_MORE_BUTTON_TEXT = 'Дізнатись більше';
 export const DO_YOU_WANT_SOMETHING_SPECIAL_DIALOGBOX_TEXT = 'Бажаєте щось особливе?';
 export const DESCRIPTION_TEXT = 'Розкажіть нам про ваші побажання. А про інше потурбується менеджер.';
+export const PHONE_FIELD_HEADER_TEXT = 'Номер телефону';
 
 //login  credential
 
