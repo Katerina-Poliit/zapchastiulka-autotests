957447ce217a2bbeaba17cd8e00f653591992a9f
Katerina Poliit <alexispoliyit@gmail.com>
2024-05-17 18:36:18 +0300

    RF_POM | productListingPage.spec | TC 03.01.48 Verify that the product card contains a photo of the product

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -114,7 +114,8 @@ class HomePage {
 		getProductCardLocator: () => this.page.getByRole('link', { name: 'Міні транспортер HECHT 2636' }),
 		getPagination: () => this.page.locator('div').filter({ hasText: /^123456789$/ }).nth(2),
 		getPaginationNextPageButton: () => this.page.getByLabel('Go to next page'),
-		getPaginationPreviousPageButton: () => this.page.locator('.MuiPagination-ul > li').first()
+		getPaginationPreviousPageButton: () => this.page.locator('.MuiPagination-ul > li').first(),
+		getProductCard: () => this.page.locator('.relative.cards:nth-child(5)')
 	};
 
 	async open() {
