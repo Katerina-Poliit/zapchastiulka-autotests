6d27710a702cdee82fb3a2ef5756bd3a94ce22b4
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-05-17 12:26:02 +0300

    RF_POM | header.spec | TC 03.01.37 Verify that the number of products of the selected category from the catalog is displayed (#141)

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -106,9 +106,8 @@ class HomePage {
 		getDeleteDataManufacturerSectionSearch: () => this.page.locator('.relative > .absolute').first(),
 		getScrollManufacturerSectionList: () => this.page.locator('#style-scroll:first-child'),
 		getTheOrderIsSuccessfulWindow: () => this.page.getByText('Замовлення успішне!Очікуйте дзвінка нашого менеджера протягом 5'),
+		getTheOrderIsSuccessfulWindow: () => this.page.getByText('Замовлення успішне!Очікуйте дзвінка нашого менеджера протягом 5'),
 		getSortDropdown: () => this.page.getByText('Сортувати:Оберіть значення')
-
-
 	};
 
 	async open() {
