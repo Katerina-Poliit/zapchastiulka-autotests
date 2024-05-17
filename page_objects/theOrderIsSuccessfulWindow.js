f7adc6682b76212e3295e174ba62d6a883798bdd
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-05-17 09:20:45 +0300

    RF_POM | header.spec | TC 03.01.68.1 Verify that the "Замовлення успішне!" modal window contains the icon (#134)

--- page_objects/theOrderIsSuccessfulWindow.js
+++ page_objects/theOrderIsSuccessfulWindow.js
@@ -5,7 +5,8 @@ class TheOrderIsSuccessfulWindowPage {
 
  locators = {
 		getSuccessfulWindowHeader: () => this.page.getByText('Замовлення успішне!'),
-		getGoToCatalogButton: () => this.page.getByRole('button', { name: 'Перейти до каталогу' })
+		getGoToCatalogButton: () => this.page.getByRole('button', { name: 'Перейти до каталогу' }),
+		getSuccessfulWindowIcon: () => this.page.locator('.modal-body > div > div > .flex')
 
 
 };
