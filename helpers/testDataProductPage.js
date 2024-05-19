d289cbbab85f9b35f8ef31e49614bf43e8c6fde7
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-05-19 11:55:22 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.14.2 Verify that the product page contains a block with the product description (#178)

--- helpers/testDataProductPage.js
+++ helpers/testDataProductPage.js
@@ -7,6 +7,7 @@
 export const PRODUCT_NAME_TEXT = 'Моторна олива Mobil Super 3000 X1 Formula FE 5W-30';
 export const PRODUCT_INFORNATION_TEXT = 'Основні характеристики:';
 export const PRODUCT_DESCRIPTION_HEADER_TEXT = 'Опис';
+export const PRODUCT_DESCRIPTION_TEXT = 'Mobil Super 3000 X1 Formula FE 5W-30 являє собою синтетичну моторну оливу, що забезпечує тривалий термін експлуатації двигунів в автомобілях різних типів і років випуску, а також підвищений рівень їх захисту в широкому діапазоні температур.';
 
 
 
