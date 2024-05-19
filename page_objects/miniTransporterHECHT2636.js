fd03839d63e7ff87def8ae615656ac6b8ab45126
Katerina Poliit <alexispoliyit@gmail.com>
2024-05-19 18:59:15 +0300

    RF_POM | productPageNotAvailable.spec | TC 04.01.59.03 Verify that the product image block contains a magnification button for the product image, a "magnifier"

--- page_objects/miniTransporterHECHT2636.js
+++ page_objects/miniTransporterHECHT2636.js
@@ -7,7 +7,8 @@ class MiniTransporterHECHT2636Page {
 		  getHECHT2636Header: () => this.page.locator('p').filter({ hasText: 'Міні транспортер HECHT' }),
 		  getHECHT2636Breadcrambs: () => this.page.locator('span').filter({ hasText: 'Міні транспортер HECHT' }),
 		  getHECHT2636ImageBlock: () => this.page.getByRole('tabpanel', { name: 'of 1' }).getByRole('img'),
-		  getHECHT2636ImageBlockMini: () => this.page.getByRole('button', { name: 'product thumbnail' })
+		  getHECHT2636ImageBlockMini: () => this.page.getByRole('button', { name: 'product thumbnail' }),
+		  getHECHT2636ImageBlocmagnifier: () => this.page.locator('div').filter({ hasText: /^Міні транспортер HECHT 2636Артикул: testProduct28$/ }).getByRole('button').first()
 
  };
 
