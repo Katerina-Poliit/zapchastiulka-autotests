3f969dc6c6cbb2191f1fd90db1eb7f5532c4fbb8
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-05-19 09:06:41 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.7 Verify that the modal window contains the image of the product (#168)

--- page_objects/modalWindowProductPageAvailable.js
+++ page_objects/modalWindowProductPageAvailable.js
@@ -5,7 +5,8 @@ class ModalWindowProductPageAvailable {
 
  locators = {
 		getModalWindow: () => this.page.locator('#modal-root div').filter({ hasText: 'Моторна олива Mobil Super' }).nth(4),
-		geProductName: () => this.page.locator('#modal-root').getByText('Моторна олива Mobil Super')
+		geProductName: () => this.page.locator('#modal-root').getByText('Моторна олива Mobil Super'),
+		geProductImage: () => this.page.locator('img[alt="Моторна олива Mobil Super 3000 X1 Formula FE 5W-30"]').last()
  };
 
 
