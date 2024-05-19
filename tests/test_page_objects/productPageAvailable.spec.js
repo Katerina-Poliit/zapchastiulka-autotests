6c189d223237285972a9afd9a98579eb7ac10d11
AlexUA2022 <avsaponov@gmail.com>
2024-05-19 10:26:49 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.11.1 Verify that the left-scroll button for scrolling images has a pointer cursor

--- tests/test_page_objects/productPageAvailable.spec.js
+++ tests/test_page_objects/productPageAvailable.spec.js
@@ -148,5 +148,17 @@ test.describe('productListPage.spec.spec', () => {
 
 	});
 
+	test('TC 04.01.11.1 Verify that the left-scroll button for scrolling images has a pointer cursor', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const mobilSuper3000Page = await homePage.clickMobilSuper3000();
+
+		const modalWindowProductPageAvailable = await mobilSuper3000Page.clickMagnifyingGlassIcon();
+
+		await expect(modalWindowProductPageAvailable.locators.getCloseButton()).toBeVisible();
+		await expect(modalWindowProductPageAvailable.locators.getButtonPreviousSlide()).toHaveCSS('cursor', 'pointer');
+
+	});
+
 
 })
