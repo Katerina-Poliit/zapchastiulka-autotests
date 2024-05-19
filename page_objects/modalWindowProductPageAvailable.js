e669eb64e8bc5fa778a4cf8ab3be79f6412c0cf6
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-05-19 09:46:53 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.8 Verify that the modal window contains the right-scroll button for scrolling images (#169)

--- page_objects/modalWindowProductPageAvailable.js
+++ page_objects/modalWindowProductPageAvailable.js
@@ -5,8 +5,9 @@ class ModalWindowProductPageAvailable {
 
  locators = {
 		getModalWindow: () => this.page.locator('#modal-root div').filter({ hasText: 'Моторна олива Mobil Super' }).nth(4),
-		geProductName: () => this.page.locator('#modal-root').getByText('Моторна олива Mobil Super'),
-		geProductImage: () => this.page.locator('img[alt="Моторна олива Mobil Super 3000 X1 Formula FE 5W-30"]').last()
+		getProductName: () => this.page.locator('#modal-root').getByText('Моторна олива Mobil Super'),
+		getProductImage: () => this.page.locator('img[alt="Моторна олива Mobil Super 3000 X1 Formula FE 5W-30"]').last(),
+		getButtonNextSlide: () => this.page.getByLabel('Next slide')
  };
 
 
