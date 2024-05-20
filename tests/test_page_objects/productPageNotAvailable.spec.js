ca2e14c1a6eaef9cf0b79c3140d51194f44fe6e9
Katerina Poliit <alexispoliyit@gmail.com>
2024-05-20 14:02:01 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.59.17 Verify that the product page contains the product description "Основні характеристики"

--- tests/test_page_objects/productPageNotAvailable.spec.js
+++ tests/test_page_objects/productPageNotAvailable.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import { BUTTON_REPORT_AVAILABILITY, MINI_TRANSPORTER_PAGE_URL,PRODUCT_NAME_TRANSPORTER,PRODUCT_TRANSPORTER_ARTICLE,PRODUCT_TRANSPORTER_PRICE } from "../../helpers/testDataProductPage.js";
+import { BUTTON_REPORT_AVAILABILITY, MINI_TRANSPORTER_PAGE_URL,PRODUCT_NAME_TRANSPORTER,PRODUCT_TRANSPORTER_ARTICLE,PRODUCT_TRANSPORTER_PRICE,PRODUCT_TRANSPORTER_MAIN_FEATURES } from "../../helpers/testDataProductPage.js";
 import MiniTransporterHECHT2636Page from "../../page_objects/miniTransporterHECHT2636.js";
 import { assert } from "console";
 
@@ -158,5 +158,14 @@ test.describe('productListPage.spec.spec', () => {
 		await page.waitForTimeout(3000);
 		await expect(cartMiniTransporterPage.locators.getPriceHECHT2636()).toBeVisible();
 		await expect(cartMiniTransporterPage.locators.getPriceHECHT2636()).toHaveText(PRODUCT_TRANSPORTER_PRICE);
+	});
+
+	test('TC 04.01.59.17 Verify that the product page contains the product description "Основні характеристики"', async ({ page}) => {
+		const homePage = new HomePage(page);
+		const cartMiniTransporterPage = await homePage.clickCardMiniTrasporterHECHT2636();
+		await page.waitForTimeout(3000);
+		await expect(cartMiniTransporterPage.locators.getDescriptionPriceHECHT2636()).toBeVisible();
+		await expect(cartMiniTransporterPage.locators.getDescriptionPriceHECHT2636()).toHaveText(PRODUCT_TRANSPORTER_MAIN_FEATURES);
+		
 	})
-})
+	})
