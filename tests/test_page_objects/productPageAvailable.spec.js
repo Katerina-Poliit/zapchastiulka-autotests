1623ffe21eca6da1e1b7b5798ff83106efbdfbae
AlexUA2022 <avsaponov@gmail.com>
2024-05-20 09:18:28 +0300

    RF_POM | productPageAvailable.spec | TC 04.01.22.1 Verify that the "Додати в кошик" button has a icon (cart)

--- tests/test_page_objects/productPageAvailable.spec.js
+++ tests/test_page_objects/productPageAvailable.spec.js
@@ -264,5 +264,15 @@ test.describe('productListPage.spec.spec', () => {
 
 	});
 
+	test('TC 04.01.22.1 Verify that the "Додати в кошик" button has a icon (cart)', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		const mobilSuper3000Page = await homePage.clickMobilSuper3000();
+
+		await expect(mobilSuper3000Page.locators.getAddToCartButton()).toBeVisible();
+		await expect(mobilSuper3000Page.locators.getIconAddToCartButton()).toBeVisible();
+
+	});
+
 
 })
