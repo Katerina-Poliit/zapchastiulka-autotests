a1af6d3dda7bb35e34c5252cb7c07a18bd6335d9
AlexUA2022 <avsaponov@gmail.com>
2024-05-27 09:15:38 +0300

    RF_POM | productCartPage.spec | TC 05.01.2 Verify that the "Empty Cart" modal window contain the "Перейти до каталогу" button, TC 05.01.3 Verify that the "Перейти до каталогу" button is colored blue, TC 05.01.4 Verify that the "Перейти до каталогу" button has the pointer cursor, TC 05.01.5 Verify that the "Empty Cart" modal window contain the close "X" button, TC 05.01.5.1 Verify that the close "X" button has the pointer cursor, TC 05.01.5.2 Verify that the "Empty Cart" modal window closes after clicking on the close "X" button, TC 05.01.6 Verify that the "Empty Cart" modal window contain the icon, TC 05.01.6.1 Verify that the "Empty Cart" modal window contain the "Кошик порожній" name, TC 05.01.6.2 Verify that the "Empty Cart" modal window contains the disabled "Очистити кошик" button, TC 05.01.6.3 Verify that the "Empty Cart" modal window contains the "Почніть додавати товари прямо зараз!" text

--- helpers/testDataProductCartPage.js
+++ helpers/testDataProductCartPage.js
@@ -6,6 +6,11 @@
 //test data
 
 export const EMPTY_CART_HEADER_TEXT = 'Кошик';
+export const GO_TO_CATALOG_BUTTON_TEXT = 'Перейти до каталогу';
+export const EMPTY_CART_NAME_TEXT = 'Кошик порожній';
+export const CLEAR_THE_CART_BUTTON_NAME_TEXT = 'Очистити кошик';
+export const MODAL_WINDOW_EMPTY_CART_TEXT = 'Почніть додавати товари прямо зараз!';
+
 
 
 
--- page_objects/modalWindowEmptyCart.js
+++ page_objects/modalWindowEmptyCart.js
@@ -7,13 +7,19 @@ class ModalWindowEmptyCart {
 
  locators = {
 		getModalWindow: () => this.page.getByText('КошикОчистити кошикКошик порожнійПочніть додавати товари прямо зараз!Перейти до '),
-		getModalWindowHeader: () => this.page.getByRole('heading', { name: 'Кошик' })
+		getModalWindowHeader: () => this.page.getByRole('heading', { name: 'Кошик' }),
+		getGoToCatalogButton: () => this.page.getByRole('button', { name: 'Перейти до каталогу' }),
+		getCloseXButton: () => this.page.locator('#modal-cart').getByRole('button').nth(1),
+		getIcon: () => this.page.locator('div').filter({ hasText: /^Кошик порожнійПочніть додавати товари прямо зараз!Перейти до каталогу$/ }).locator('div').first(),
+		getEmptyCartName: () => this.page.getByText('Кошик порожній'),
+		getClearTheCartButton: () => this.page.getByRole('button', { name: 'Очистити кошик' }),
+		getModalWindowEmptyCartText: () => this.page.getByText('Почніть додавати товари прямо зараз!')
  };
 
-		// async clickGoToTheCatalogButton() {
-		// 	await this.locators.getGoToTheCatalogButton().click();
-		// 	return new HomePage(this.page);
-		// }
+		async clickCloseXButton() {
+			await this.locators.getCloseXButton().click();
+			return new HomePage(this.page);
+		}
 
 }
 
