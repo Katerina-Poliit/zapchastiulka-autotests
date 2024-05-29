1361e68a1a3227ef1807c220c4d8a4cdaa207136
AlexUA2022 <avsaponov@gmail.com>
2024-05-29 11:28:49 +0300

    RF_POM | checkoutPage.spec.js | TC 05.01.115 Verify that the "Контактнi данi" block contains the "ЄДРПОУ *" mandatory field, TC 05.01.116 Verify that the "Контактнi данi" block contains the "ІПН *" mandatory field, TC 05.01.117 Verify that the "Контактнi данi" block contains the "Область реєстрації *" mandatory field, TC 05.01.117.1 Verify that the "Область реєстрації *" mandatory field conreins the "Введіть назву області.." placeholder, TC 05.01.118 Verify that the "Контактнi данi" block contains the "Місто реєстрації *" mandatory field, TC 05.01.118.1 Verify that the "Місто реєстрації *" mandatory field conreins the "Введіть назву міста.." placeholder, TC 05.01.119 Verify that the "Контактнi данi" block contains the "Юридична адреса *" mandatory field, TC 05.01.120 Verify that the "Контактнi данi" block contains the "Ім\'я *" mandatory field, TC 05.01.121 Verify that the "Контактнi данi" block contains the "Прізвище *" mandatory field, TC 05.01.122 Verify that the "Контактнi данi" block contains the "По батькові" field

--- tests/test_page_objects/checkoutPage.spec.js
+++ tests/test_page_objects/checkoutPage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import { LEGAL_ENTITY_BUTTON_TEXT, TYPE_OF_RESTRAINT_FIELD_LABEL_TEXT, CONTACT_INFORMATION_BLOCK_HEADER_TEXT, LEGAL_ENTITY_SECTION_TEXT, FOP_SECTION_TEXT, NAME_FIELD_LABEL_TEXT } from "../../helpers/testDataProductCartPage.js";
+import { LEGAL_ENTITY_BUTTON_TEXT, TYPE_OF_RESTRAINT_FIELD_LABEL_TEXT, CONTACT_INFORMATION_BLOCK_HEADER_TEXT, LEGAL_ENTITY_SECTION_TEXT, FOP_SECTION_TEXT, NAME_FIELD_LABEL_TEXT, EDRPOU_FIELD_LABEL_TEXT, IPN_FIELD_LABEL_TEXT, REGION_FIELD_LABEL_TEXT, CITY_FIELD_LABEL_TEXT, LEGAL_ADDRESSES_FIELD_LABEL_TEXT, FIRST_NAME_FIELD_LABEL_TEXT, LAST_NAME_FIELD_LABEL_TEXT, MIDDLE_NAME_FIELD_LABEL_TEXT } from "../../helpers/testDataProductCartPage.js";
 import CheckoutPage from "../../page_objects/checkoutPage.js";
 
 test.describe('checkoutPage.spec', () => {
@@ -116,6 +116,118 @@ test.describe('checkoutPage.spec', () => {
 		await expect(checkoutPage.locators.getNameField()).toBeVisible();
 		await expect(checkoutPage.locators.getNameFieldLabel()).toBeVisible();
 		await expect(checkoutPage.locators.getNameFieldLabel()).toHaveText(NAME_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.115 Verify that the "Контактнi данi" block contains the "ЄДРПОУ *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getEDRPOUField()).toBeVisible();
+		await expect(checkoutPage.locators.getEDRPOUFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getEDRPOUFieldLabel()).toHaveText(EDRPOU_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.116 Verify that the "Контактнi данi" block contains the "ІПН *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getIPNField()).toBeVisible();
+		await expect(checkoutPage.locators.getIPNFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getIPNFieldLabel()).toHaveText(IPN_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.117 Verify that the "Контактнi данi" block contains the "Область реєстрації *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getRegionField()).toBeVisible();
+		await expect(checkoutPage.locators.getRegionFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getRegionFieldLabel()).toHaveText(REGION_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.117.1 Verify that the "Область реєстрації *" mandatory field conreins the "Введіть назву області.." placeholder', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		const regionField = checkoutPage.locators.getRegionField();
+
+		await expect(regionField).toBeVisible(); 
+		await expect(regionField).toHaveAttribute('placeholder', 'Введіть назву області..');
+
+	});
+
+	test('TC 05.01.118 Verify that the "Контактнi данi" block contains the "Місто реєстрації *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getCityField()).toBeVisible();
+		await expect(checkoutPage.locators.getCityFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getCityFieldLabel()).toHaveText(CITY_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.118.1 Verify that the "Місто реєстрації *" mandatory field conreins the "Введіть назву міста.." placeholder', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		const regionField = checkoutPage.locators.getCityField();
+
+		await expect(regionField).toBeVisible(); 
+		await expect(regionField).toHaveAttribute('placeholder', 'Введіть назву міста..');
+
+	});
+
+	test('TC 05.01.119 Verify that the "Контактнi данi" block contains the "Юридична адреса *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getLegalAddressesField()).toBeVisible();
+		await expect(checkoutPage.locators.getLegalAddressesFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getLegalAddressesFieldLabel()).toHaveText(LEGAL_ADDRESSES_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.120 Verify that the "Контактнi данi" block contains the "Ім\'я *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getFirstNameField()).toBeVisible();
+		await expect(checkoutPage.locators.getFirstNameFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getFirstNameFieldLabel()).toHaveText(FIRST_NAME_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.121 Verify that the "Контактнi данi" block contains the "Прізвище *" mandatory field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getLastNameField()).toBeVisible();
+		await expect(checkoutPage.locators.getLastNameFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getLastNameFieldLabel()).toHaveText(LAST_NAME_FIELD_LABEL_TEXT);
+
+	});
+
+	test('TC 05.01.122 Verify that the "Контактнi данi" block contains the "По батькові" field', async ({ page }) => {
+		const checkoutPage = new CheckoutPage(page);
+
+		await checkoutPage.clickLegalEntityButton();
+
+		await expect(checkoutPage.locators.getMiddleNameField()).toBeVisible();
+		await expect(checkoutPage.locators.getMiddleNameFieldLabel()).toBeVisible();
+		await expect(checkoutPage.locators.getMiddleNameFieldLabel()).toHaveText(MIDDLE_NAME_FIELD_LABEL_TEXT);
 
 	});
 
